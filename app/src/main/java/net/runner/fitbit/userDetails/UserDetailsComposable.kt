98a1da26a150523e7cdfada1a1992482800a8ae4
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-08 06:35:25 +0530

    <New:Init>?

---
+++ app/src/main/java/net/runner/fitbit/userDetails/UserDetailsComposable.kt
@@ -0,0 +1,16 @@
+package net.runner.fitbit.userDetails
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import net.runner.fitbit.ui.theme.background
+
+@Composable
+fun UserDetailComposable(navController: NavController) {
+    Box(modifier = Modifier.fillMaxSize().background(background)){
+
+    }
+}
