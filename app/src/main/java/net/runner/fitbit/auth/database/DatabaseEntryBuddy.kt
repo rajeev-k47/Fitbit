00c7bcf4687db18f0df97b9301f8e64a514977c3
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-08 22:09:33 +0530

    <Feat,new:Database entry for buddy to firestore and confirmation to sharedpreferences>?

---
+++ app/src/main/java/net/runner/fitbit/auth/database/DatabaseEntryBuddy.kt
@@ -0,0 +1,48 @@
+package net.runner.fitbit.auth.database
+
+import android.content.Context
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+fun DatabaseEntryBuddy(
+    goalType: Set<String>,
+    selectedStartTime: String,
+    selectedEndTime: String,
+    username: String,
+    email: String,
+    gender: String,
+    timeFrame: String,
+    targetValue: String,
+    context: Context,
+    onresult:(String)-> Unit
+) {
+    val auth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
+
+    val userData = hashMapOf(
+        "goalType" to goalType.toList(),
+        "selectedStartTime" to selectedStartTime,
+        "selectedEndTime" to selectedEndTime,
+        "username" to username,
+        "email" to email,
+        "gender" to gender,
+        "timeFrame" to timeFrame,
+        "targetValue" to targetValue
+    )
+
+    db.collection("users")
+        .document(auth.uid.toString())
+        .set(userData)
+        .addOnSuccessListener { documentReference ->
+           onresult("success")
+            val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("user_exists_${auth.uid}", true.toString())
+            editor.apply()
+
+        }
+        .addOnFailureListener { e ->
+            onresult("failure")
+        }
+}
