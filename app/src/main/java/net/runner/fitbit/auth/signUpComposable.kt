936c2d4093245b97ef8f6bda0ec7d6ccbb071bac
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-08 05:44:24 +0530

    <New,Init:Initiated only UI for signUp page>?

---
+++ app/src/main/java/net/runner/fitbit/auth/signUpComposable.kt
@@ -0,0 +1,200 @@
+package net.runner.fitbit.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import net.runner.fitbit.R
+import net.runner.fitbit.ui.theme.background
+import net.runner.fitbit.ui.theme.lightBlueText
+import net.runner.fitbit.ui.theme.lightText
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpComposable(navController: NavController) {
+
+    var emailText by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(background)
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.statusBars)
+
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(0.3f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(painter = painterResource(id = R.drawable.logo_heading_cropped), contentDescription = "" )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(0.7f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Column (
+                    modifier = Modifier.fillMaxWidth(0.8f)
+                    ,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Text(
+                        text = "Sign up",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.size(10.dp))
+                    Text(
+                        text = "Once you sign up, your personal feed will be ready to explore.",
+                        color = lightText,
+                        fontSize = 17.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            ,
+                        shape = RoundedCornerShape(12.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.White
+                        )
+
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(12.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Image(painter = painterResource(id = R.drawable.google), contentDescription = "Google_default",Modifier.size(22.dp))
+                            Spacer(modifier = Modifier.size(4.dp))
+                            Text(text = "Google", color = Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(start = 10.dp))
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(15.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Box(modifier = Modifier.weight(0.4f)){
+                            HorizontalDivider(thickness = 1.1.dp, color = lightText.copy(alpha = 0.3f))
+                        }
+                        Box(modifier = Modifier.weight(0.1f)){
+                            Text(text = "or", fontWeight = FontWeight.Bold , color = Color.White, fontSize = 16.sp, modifier = Modifier.align(Alignment.Center))
+                        }
+                        Box(modifier = Modifier.weight(0.4f)){
+                            HorizontalDivider(thickness = 1.1.dp, color = lightText.copy(alpha = 0.4f))
+                        }
+                    }
+                    Spacer(modifier = Modifier.size(15.dp))
+                        TextField(
+                            value = emailText,
+                            onValueChange = {text-> emailText = text},
+                            placeholder = {
+                            Text(text = "Email", color = lightBlueText, fontSize = 17.sp)
+                            },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = lightText.copy(alpha = 0.1f),
+                                unfocusedContainerColor = lightText.copy(alpha = 0.1f),
+                                focusedIndicatorColor = Color.Red.copy(alpha = 0.5f),
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                            ),
+                            leadingIcon = {
+                                Icon(painter = painterResource(id = R.drawable.mail), contentDescription = "", modifier = Modifier.size(24.dp), tint = lightBlueText)
+                            },
+                            trailingIcon ={
+                                Button(
+                                    onClick = { /*TODO*/ },
+                                    shape = RoundedCornerShape(12.dp),
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = lightText.copy(alpha = 0.2f),
+                                    ),
+                                    contentPadding = PaddingValues(0.dp),
+                                    modifier = Modifier
+                                        .padding(horizontal = 10.dp)
+                                        .size(40.dp)
+                                ) {
+                                    Icon(painter = painterResource(id = R.drawable.arrow_right), contentDescription = "signUp", modifier = Modifier.size(22.dp), tint = lightBlueText)
+                                }
+                            }
+                        )
+                    Spacer(modifier = Modifier.size(15.dp))
+                    HorizontalDivider(modifier = Modifier.fillMaxWidth(), thickness = 1.1.dp, color = lightText.copy(alpha = 0.3f))
+                    Spacer(modifier = Modifier.size(15.dp))
+
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(text = "Already using Fitbit? ", color = lightText, fontSize = 16.sp)
+                        Text(text = "Log in", color = Color.White, fontSize = 16.sp, modifier = Modifier.clickable {
+//                            navController.navigate("login")
+                        },
+                            textDecoration = TextDecoration.Underline
+                            )
+                    }
+
+                }
+            }
+        }
+    }
+}
