edab9b32f6f7a825b75aef2f612d99f8e1ac5131
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-10 03:35:31 +0530

    <New:Toast lib>?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,5 +92,6 @@ dependencies {
     // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:21.3.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation ("io.github.shashank02051997:FancyToast:2.0.2")
 
 }
