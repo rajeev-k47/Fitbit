6c6603b88a326351a3b93c41ab633c5e09513216
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-10 03:37:26 +0530

    <Refac:Toast,Warning>?

--- app/src/main/java/net/runner/fitbit/auth/signUpComposable.kt
+++ app/src/main/java/net/runner/fitbit/auth/signUpComposable.kt
@@ -18,8 +18,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -49,6 +52,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
+import com.shashank.sony.fancytoastlib.FancyToast
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import net.runner.fitbit.R
@@ -65,6 +69,9 @@ fun SignUpComposable(navController: NavController,activity: Activity) {
     val auth = FirebaseAuth.getInstance()
     var accountstatus by rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
+    var wrongEmailWarning by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     DisposableEffect(auth) {
         val authListener = FirebaseAuth.AuthStateListener{ auth ->
@@ -214,15 +221,34 @@ fun SignUpComposable(navController: NavController,activity: Activity) {
                             leadingIcon = {
                                 Icon(painter = painterResource(id = R.drawable.mail), contentDescription = "", modifier = Modifier.size(24.dp), tint = lightBlueText)
                             },
+                            supportingText = {
+                                if(wrongEmailWarning){
+
+                                    Row (
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.Start,
+                                        verticalAlignment = Alignment.CenterVertically
+                                    ){
+                                        Icon(imageVector = Icons.Filled.Warning, contentDescription = "", tint = Color.Red, modifier = Modifier.size(18.dp))
+                                        Spacer(modifier = Modifier.width(5.dp))
+                                        Text(text = "Enter a vaild email", color = Color.Red, fontSize = 17.sp, fontWeight = FontWeight.Bold)
+                                    }
+                                }
+                            },
                             trailingIcon ={
                                 Button(
                                     onClick = {
                                         if(Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+                                            wrongEmailWarning = false
                                             coroutineScope.launch{
                                                 EmailLinkAuth(emailText)
                                                 tempEmailSignUp(emailText,context)
                                             }
+                                            FancyToast.makeText(context,"Email sent !!",FancyToast.LENGTH_LONG,FancyToast.DEFAULT,false).show()
+                                        }else{
+                                            wrongEmailWarning = true
                                         }
+
                                     },
                                     shape = RoundedCornerShape(12.dp),
                                     colors = ButtonDefaults.buttonColors(
@@ -237,22 +263,22 @@ fun SignUpComposable(navController: NavController,activity: Activity) {
                                 }
                             }
                         )
-                    Spacer(modifier = Modifier.size(15.dp))
-                    HorizontalDivider(modifier = Modifier.fillMaxWidth(), thickness = 1.1.dp, color = lightText.copy(alpha = 0.3f))
-                    Spacer(modifier = Modifier.size(15.dp))
-
-                    Row (
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ){
-                        Text(text = "Already using Fitbit? ", color = lightText, fontSize = 16.sp)
-                        Text(text = "Log in", color = Color.White, fontSize = 16.sp, modifier = Modifier.clickable {
-//                            navController.navigate("login")
-                        },
-                            textDecoration = TextDecoration.Underline
-                            )
-                    }
+                    Spacer(modifier = Modifier.size(75.dp))
+//                    HorizontalDivider(modifier = Modifier.fillMaxWidth(), thickness = 1.1.dp, color = lightText.copy(alpha = 0.3f))
+//                    Spacer(modifier = Modifier.size(15.dp))
+//
+//                    Row (
+//                        modifier = Modifier.fillMaxWidth(),
+//                        horizontalArrangement = Arrangement.Center,
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ){
+//                        Text(text = "Already using Fitbit? ", color = lightText, fontSize = 16.sp)
+//                        Text(text = "Log in", color = Color.White, fontSize = 16.sp, modifier = Modifier.clickable {
+////                            navController.navigate("login")
+//                        },
+//                            textDecoration = TextDecoration.Underline
+//                            )
+//                    }
 
                 }
             }
