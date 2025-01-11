539c4d36cc8c9d79c96bdee793492698dad29259
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-12 04:49:30 +0530

    <Refac:Refac navigation>?

--- app/src/main/java/net/runner/fitbit/WorkoutBuddyDashBoard/WorkoutBuddyDashBoard.kt
+++ app/src/main/java/net/runner/fitbit/WorkoutBuddyDashBoard/WorkoutBuddyDashBoard.kt
@@ -4,34 +4,52 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import net.runner.fitbit.WorkoutBuddyDashBoard.BottomNavigationbar.BottomNavigationbarComposable
-import net.runner.fitbit.WorkoutBuddyDashBoard.TopAppBar.TopAppBarComposable
+import net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.ExploreFragment.ExploreFragmentComposable
+import net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.HomeFragment.HomeFragment
 import net.runner.fitbit.ui.theme.background
 import net.runner.fitbit.ui.theme.lightText
 
 @Composable
 fun WorkoutBuddyDashBoard(navController:NavController){
+    
+    var currentFragment by rememberSaveable { mutableStateOf("Home") }
+    
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(background)){
-
-        LazyColumn(
-            modifier = Modifier.fillMaxSize().statusBarsPadding(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item {
-                TopAppBarComposable(navController)
+        .background(background)
+        .statusBarsPadding()){
+        when (currentFragment) {
+            "Home" -> {
+                HomeFragment(navController = navController)
             }
-
+            "Explore" -> {
+                ExploreFragmentComposable()
+            }
+            "Activity" -> {
+                Text(text = "Activity", color = lightText)
+            }
+            "Groups" -> {
+                
+            }
+            "Create" -> {
+                Text(text = "Create", color = lightText)
+            }
         }
+       
 
-        BottomNavigationbarComposable(Modifier.align(Alignment.BottomCenter))
+        BottomNavigationbarComposable(Modifier.align(Alignment.BottomCenter)){
+            currentFragment = it
+        }
 
     }
 }
