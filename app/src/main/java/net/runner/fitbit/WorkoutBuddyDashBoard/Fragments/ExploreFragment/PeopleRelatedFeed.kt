6eb1bf7b5867631ef5697f747c2ed658269a8f1f
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-12 04:45:49 +0530

    <New,Feat,Init:Init people-related-feed logic filters based on (goalType)>?

---
+++ app/src/main/java/net/runner/fitbit/WorkoutBuddyDashBoard/Fragments/ExploreFragment/PeopleRelatedFeed.kt
@@ -0,0 +1,36 @@
+package net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.ExploreFragment
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+
+fun PeopleRelatedFeed(
+    currentUserGoals: List<String>,
+    onResult: (List<Pair<Map<String, Any>, Int>>) -> Unit
+) {
+    val db = FirebaseFirestore.getInstance()
+    val usersCollection = db.collection("users")
+
+    usersCollection.get()
+        .addOnSuccessListener { querySnapshot ->
+            val similarUsers = mutableListOf<Pair<Map<String, Any>, Int>>()
+
+            for (document in querySnapshot.documents) {
+                val userData = document.data
+                if(userData?.get("email")== FirebaseAuth.getInstance().currentUser?.email)continue
+                val userGoals = userData?.get("goalType") as? List<String>?:continue
+                val matchingGoals = userGoals.intersect(currentUserGoals.toSet())
+                val similarityScore = matchingGoals.size
+
+                if (similarityScore > 0) {
+                    similarUsers.add(userData to similarityScore)
+                }
+            }
+            val sortedSimilarUsers = similarUsers.sortedByDescending { it.second }
+            onResult(sortedSimilarUsers)
+        }
+        .addOnFailureListener { exception ->
+            Log.e("Firestore", "Error fetching users: ${exception.message}")
+        }
+}
