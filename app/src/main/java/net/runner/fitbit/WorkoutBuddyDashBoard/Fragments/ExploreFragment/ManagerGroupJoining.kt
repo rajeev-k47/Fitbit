540684013c330b53e74f7e6058146bb6a4782f9b
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-19 01:40:18 +0530

    <Feat:Save groupid to user database and userid to group database>?

--- app/src/main/java/net/runner/fitbit/WorkoutBuddyDashBoard/Fragments/ExploreFragment/ManagerGroupJoining.kt
+++ app/src/main/java/net/runner/fitbit/WorkoutBuddyDashBoard/Fragments/ExploreFragment/ManagerGroupJoining.kt
@@ -3,9 +3,10 @@ package net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.ExploreFragment
 import com.google.firebase.firestore.FirebaseFirestore
 import net.runner.fitbit.Database.GroupStatus
 import net.runner.fitbit.Database.SaveAndUpdateBuddyGroups
+import net.runner.fitbit.Database.addUserToGroup
 import net.runner.fitbit.ServerSide.makeGroupJoinRequest
 
-fun ManagerGroupJoining(userId: String,groupId: String,onresult:()->Unit) {
+fun ManagerGroupJoining(userId: String,groupId: String,onresult:(Boolean)->Unit) {
     val db = FirebaseFirestore.getInstance()
     val groupCollection = db.collection("users")
     val groupDocument = groupCollection.document(groupId)
@@ -17,9 +18,12 @@ fun ManagerGroupJoining(userId: String,groupId: String,onresult:()->Unit) {
 
             if (isPrivate) {
                 makeGroupJoinRequest(userId, groupId)
+                SaveAndUpdateBuddyGroups(userId, GroupStatus(groupId, "Pending"))//for user database entry that it is pending or not and to make easily accessible to show in group fragments
+                onresult(true)
             }else{
                 SaveAndUpdateBuddyGroups(userId, GroupStatus(groupId, "Accepted"))
-                onresult()
+                addUserToGroup(userId, groupId)//for organizer to control the users of the group
+                onresult(false)
             }
         } else {
             println("No such group")
