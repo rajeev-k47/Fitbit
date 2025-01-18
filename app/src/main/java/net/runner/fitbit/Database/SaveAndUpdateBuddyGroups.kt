aadb038235d104c5fb0be7855c882853e69d94db
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-18 18:49:26 +0530

    <New,Init:Init func fro saving and updating group status >?

---
+++ app/src/main/java/net/runner/fitbit/Database/SaveAndUpdateBuddyGroups.kt
@@ -0,0 +1,62 @@
+package net.runner.fitbit.Database
+
+import androidx.compose.runtime.Composable
+import com.google.firebase.firestore.FirebaseFirestore
+
+data class GroupStatus(
+    val groupId: String = "",
+    val status: String =""
+)
+fun SaveAndUpdateBuddyGroups(userId: String, group:GroupStatus) {
+
+        val firestore = FirebaseFirestore.getInstance()
+
+    val userDocument = firestore.collection("users").document(userId)
+
+    userDocument.get()
+        .addOnSuccessListener { document ->
+            if (document.exists()) {
+                val userData = document.data
+                println( userData?.get("groups"))
+                val groups = userData?.get("groups") as? MutableList<GroupStatus> ?: mutableListOf()
+                if (!groups.any { it.groupId == group.groupId }) {
+                    groups.add(group)
+                }
+
+                userDocument.update("groups", groups)
+                    .addOnSuccessListener {
+                        println("Group added successfully")
+                    }
+                    .addOnFailureListener { e ->
+                        e.printStackTrace()
+                        println("Error updating groups: ${e.message}")
+                    }
+            } else {
+                println("User document does not exist")
+            }
+        }
+        .addOnFailureListener { e ->
+            e.printStackTrace()
+            println("Error fetching user data: ${e.message}")
+        }
+}
+fun updateGroupStatus(userId: String, groupId: String, newStatus: String) {
+    val firestore = FirebaseFirestore.getInstance()
+
+    firestore.collection("users")
+        .document(userId)
+        .get()
+        .addOnSuccessListener { document ->
+            if (document.exists()) {
+                val userData = document.data
+                val groups = userData?.get("groups") as? List<GroupStatus> ?: emptyList()
+                val updatedGroups = groups.map { if (it.groupId == groupId) it.copy(status = newStatus) else it }
+
+//                saveGroupStatusToFirebase(userId, updatedGroups)
+            }
+        }
+        .addOnFailureListener { e ->
+            e.printStackTrace()
+            println("Error fetching user data: ${e.message}")
+        }
+}
