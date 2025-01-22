5a56904cc248f50ed1f46d27a963e5d61fd99192
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-23 03:58:36 +0530

    <Feat:Feed like status func>?

--- app/src/main/java/net/runner/fitbit/UserFeed/FeedManager.kt
+++ app/src/main/java/net/runner/fitbit/UserFeed/FeedManager.kt
@@ -2,6 +2,7 @@ package net.runner.fitbit.UserFeed
 
 import android.os.Parcelable
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.parcel.Parcelize
 import java.text.SimpleDateFormat
 
@@ -22,16 +23,16 @@ import java.text.SimpleDateFormat
 fun getPosts(callback: (List<Map<String, Any>>) -> Unit){
     val db = FirebaseFirestore.getInstance()
     val postsCollection = db.collection("posts")
-    postsCollection.get()
-        .addOnSuccessListener { documents ->
+    postsCollection
+        .orderBy("dateTime", Query.Direction.DESCENDING)
+        .addSnapshotListener { snapshot, e ->
             val postsList = mutableListOf<Map<String, Any>>()
-            for (document in documents) {
-                postsList.add(document.data)
+            if (snapshot != null) {
+                for (document in snapshot) {
+                    postsList.add(document.data)
+                }
             }
             callback(postsList)
-        }
-        .addOnFailureListener { exception ->
-            //error
         }
 }
 
@@ -40,4 +41,20 @@ fun getFormatedTimeFeed(dateTime: String): String {
     val date = sdf.parse(dateTime)
     val sdf2 = SimpleDateFormat("dd MMM yyyy, hh:mm a")
     return sdf2.format(date)
+}
+fun manageLikeStatus(postId: String, userId: String) {
+    val db = FirebaseFirestore.getInstance()
+    val postDocRef = db.collection("posts").document(postId)
+    postDocRef.get()
+        .addOnSuccessListener { documentSnapshot ->
+            val likes = documentSnapshot.get("likes") as? List<String> ?: emptyList()
+            val isLiked = likes.contains(userId)
+            if(isLiked){
+                postDocRef.update("likes", likes - userId)
+            }else{
+                postDocRef.update("likes", likes + userId)
+            }
+        }
+        .addOnFailureListener { exception ->
+        }
 }
