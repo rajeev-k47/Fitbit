587281518356e2a847467ca74b4c9454bc7ce65f
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-23 18:19:47 +0530

    <New,Init:Firebase Messaging service init >?

---
+++ app/src/main/java/net/runner/fitbit/FirebaseMessagingService.kt
@@ -0,0 +1,55 @@
+package net.runner.fitbit
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d("FCM", "Message received: ${remoteMessage.data}")
+        remoteMessage.notification?.let {
+            showNotification(it.title ?: "Notification", it.body ?: "You have a new request")
+            println(it.body)
+        }
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val channelId = "default_channel_id"
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Default Chanel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.fitbit_logo)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+}
