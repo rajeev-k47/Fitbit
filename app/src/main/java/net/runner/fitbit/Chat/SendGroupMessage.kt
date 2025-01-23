8e52cfd903dbc841ce668418fb608d304d297827
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-01-23 18:21:08 +0530

    <New,Init:Group Message Sending>?

---
+++ app/src/main/java/net/runner/fitbit/Chat/SendGroupMessage.kt
@@ -0,0 +1,28 @@
+package net.runner.fitbit.Chat
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
+fun sendGroupMessage(groupUid: String, content: String) {
+    val userId = FirebaseAuth.getInstance().currentUser?.uid ?: return
+    val db = FirebaseFirestore.getInstance()
+
+
+    val message = Message(
+        timestamp = System.currentTimeMillis(),
+        userId = userId,
+        content = content
+    )
+
+    db.collection("Chats")
+        .document(groupUid)
+        .collection("messages")
+        .add(message)
+        .addOnSuccessListener { documentReference ->
+            println("Message  ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            println("Error message: $e")
+        }
+}
+
