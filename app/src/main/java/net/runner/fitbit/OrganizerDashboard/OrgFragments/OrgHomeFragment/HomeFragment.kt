cea448b5265f313b211950f5ace3692bfc8fb50f
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-02-11 04:06:49 +0530

    <Refac:*/*>?

--- app/src/main/java/net/runner/fitbit/OrganizerDashboard/OrgFragments/OrgHomeFragment/HomeFragment.kt
+++ app/src/main/java/net/runner/fitbit/OrganizerDashboard/OrgFragments/OrgHomeFragment/HomeFragment.kt
@@ -3,6 +3,8 @@ package net.runner.fitbit.OrganizerDashboard.OrgFragments.OrgHomeFragment
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,11 +13,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -41,6 +45,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,6 +57,7 @@ import com.toolsforfools.shimmery.shimmerConfiguration.ShimmerType
 import com.toolsforfools.shimmery.shimmerIndividual.shimmer
 import net.runner.fitbit.GroupPanel.GroupContent
 import net.runner.fitbit.OrganizerDashboard.TopAppBar.OrgTopAppBarComposable
+import net.runner.fitbit.Profiles.getFormatedTime
 import net.runner.fitbit.R
 import net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.GroupsFragment.GetGroupData
 import net.runner.fitbit.ui.theme.background
@@ -72,7 +78,91 @@ fun OrgHomeFragment(navController: NavController) {
 
         }
     }
+
+    val scrollstate = rememberLazyListState()
+    LaunchedEffect(groupData) {
+        if(groupData.size>0){
+            scrollstate.scrollToItem(groupData.size - 1)
+        }
+    }
+
     Box(modifier = Modifier.fillMaxSize()){
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            state = scrollstate
+        ) {
+            item {
+                OrgTopAppBarComposable(navController =navController)
+            }
+            item {
+                Spacer(modifier = Modifier.height(100.dp))
+            }
+            item {
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    SubcomposeAsyncImage(
+                        model = groupData["profileImageUrl"],
+                        contentDescription = "Profile Picture",
+                        loading = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .shimmer(true) {
+                                        shimmerType = ShimmerType.WITH_ALPHA_AND_GRADIANT
+                                        gradientType = GradientType.LINEAR
+                                        shape = RoundedCornerShape(25.dp)
+                                        gradientAnimationSpec = tween(1000)
+                                        alphaAnimationSpec = tween(1300)
+                                    },
+                            )
+
+                        },
+                        error = {
+                            Image(
+                                painter = painterResource(id = R.drawable.user),
+                                contentDescription = "avatar",
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(25.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .size(105.dp)
+                            .clip(RoundedCornerShape(25.dp))
+                            .border(0.5.dp, Color.White, RoundedCornerShape(25.dp))
+                            .clickable {
+//                            navController.navigate("profileBuddy")
+                            },
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(text = "${(groupData["groupData"] as? Map<*, *>?)?.get("organizationName")}", color = Color.White, fontSize = 16.sp, fontWeight = FontWeight.Bold, overflow = TextOverflow.Ellipsis, maxLines = 1, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(text = "@${(groupData["groupData"] as? Map<*, *>?)?.get("organizerName")} • Created ${getFormatedTime(groupData["joiningDate"].toString())} ", color = lightText, fontSize = 12.sp, fontWeight = FontWeight.Bold, overflow = TextOverflow.Ellipsis, maxLines = 1, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                        textAlign = TextAlign.Center
+                    )
+                    Text(text = "Total Members : ${(groupData["users"] as? List<String>)?.size}", color = lightText, fontSize = 12.sp, fontWeight = FontWeight.Bold, overflow = TextOverflow.Ellipsis, maxLines = 1, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                        textAlign = TextAlign.Center
+                    )
+
+                }
+
+            }
+
+        }
         ExtendedFloatingActionButton(
             onClick = {  },
             icon = { Icon(Icons.Filled.Add, "") },
@@ -83,29 +173,6 @@ fun OrgHomeFragment(navController: NavController) {
             containerColor = lightBlueText.copy(0.5f),
             contentColor = Color.White,
         )
-
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item {
-                OrgTopAppBarComposable(navController =navController)
-            }
-            item {
-                Box (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(background)
-                ){
-
-
-
-                }
-
-            }
-
-        }
-        GroupContent(groupData = groupData)
     }
 
 
