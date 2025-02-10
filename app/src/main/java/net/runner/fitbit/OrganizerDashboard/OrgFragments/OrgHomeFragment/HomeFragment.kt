f35ff73d8e8ad414b879643bcf11c05156a12eae
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-02-11 03:44:37 +0530

    <Refac:*/*>?

--- app/src/main/java/net/runner/fitbit/OrganizerDashboard/OrgFragments/OrgHomeFragment/HomeFragment.kt
+++ app/src/main/java/net/runner/fitbit/OrganizerDashboard/OrgFragments/OrgHomeFragment/HomeFragment.kt
@@ -1,22 +1,112 @@
 package net.runner.fitbit.OrganizerDashboard.OrgFragments.OrgHomeFragment
 
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.SubcomposeAsyncImage
+import com.google.firebase.auth.FirebaseAuth
+import com.toolsforfools.shimmery.shimmerConfiguration.GradientType
+import com.toolsforfools.shimmery.shimmerConfiguration.ShimmerType
+import com.toolsforfools.shimmery.shimmerIndividual.shimmer
+import net.runner.fitbit.GroupPanel.GroupContent
 import net.runner.fitbit.OrganizerDashboard.TopAppBar.OrgTopAppBarComposable
+import net.runner.fitbit.R
+import net.runner.fitbit.WorkoutBuddyDashBoard.Fragments.GroupsFragment.GetGroupData
+import net.runner.fitbit.ui.theme.background
+import net.runner.fitbit.ui.theme.lightBlueText
+import net.runner.fitbit.ui.theme.lightText
 
 @Composable
 fun OrgHomeFragment(navController: NavController) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        item {
-            OrgTopAppBarComposable(navController =navController)
-        }
-
+    var groupData by rememberSaveable {
+        mutableStateOf(mapOf<String, Any>())
     }
+    val groupId = FirebaseAuth.getInstance().currentUser?.uid!!
+
+    LaunchedEffect (Unit){
+
+        GetGroupData(groupId = groupId) {
+            groupData = it
+
+        }
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        ExtendedFloatingActionButton(
+            onClick = {  },
+            icon = { Icon(Icons.Filled.Add, "") },
+            text = { Text(text = "New Event") },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 100.dp, end = 20.dp),
+            containerColor = lightBlueText.copy(0.5f),
+            contentColor = Color.White,
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                OrgTopAppBarComposable(navController =navController)
+            }
+            item {
+                Box (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(background)
+                ){
+
+
+
+                }
+
+            }
+
+        }
+        GroupContent(groupData = groupData)
+    }
+
+
 }
